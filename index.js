6afb901f8c24e2887663d93b528be20e6e3685c7
Jason Lesslie <jason.lesslie@gmail.com>
2018-01-14 04:00:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function prependKitten(name) {
   return outputArray
 }
 function removeLastKitten() {
-  return kittens.slice(0, kitens.length - 1)
+  return kittens.slice(0, kittens.length - 1)
 }
 function removeFirstKitten() {
   return kittens.slice(1)
